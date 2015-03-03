1b997ae079e39240d1ea3e81481d92c24047b9d8
Marcio Trindade <marciotrindade@gmail.com>
2015-03-03 18:31:16 -0300

    write teste to custom error message

--- spec/heartcheck/checks/activerecord_spec.rb
+++ spec/heartcheck/checks/activerecord_spec.rb
@@ -1,6 +1,7 @@
 describe Heartcheck::Checks::Activerecord do
-  let(:conn)    { ActiveRecord::Base.connection }
+  let(:conn) { ActiveRecord::Base.connection }
   let(:service) { { name: 'Default', connection: conn } }
+  let(:check_errors) { subject.instance_variable_get(:@errors) }
 
   subject do
     described_class.new.tap { |c| c.add_service(service) }
@@ -9,7 +10,12 @@ describe Heartcheck::Checks::Activerecord do
   describe '#validate' do
     context 'with success' do
       before do
-        ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'spec/db/test.sqlite3')
+        conn_settings = {
+          adapter: 'sqlite3',
+          database: 'spec/db/test.sqlite3'
+        }
+
+        ActiveRecord::Base.establish_connection(conn_settings)
       end
 
       after do
@@ -19,14 +25,22 @@ describe Heartcheck::Checks::Activerecord do
       it "doesn't append error" do
         expect(conn).to receive(:execute).and_call_original
         subject.validate
-        expect(subject.instance_variable_get(:@errors)).to be_empty
+        expect(check_errors).to be_empty
       end
     end
 
     context 'with error' do
-      it 'appends error' do
+      it 'appends default error message' do
         subject.validate
-        expect(subject.instance_variable_get(:@errors)).to eq(['Error to use the "Default" connection'])
+        expect(check_errors).to eq(['Error to use the "Default" connection'])
+      end
+
+      it 'appends custom error message' do
+        subject.on_error do |errors, service|
+          errors << "Activerecord #{service[:name]} is not connected"
+        end
+        subject.validate
+        expect(check_errors).to eq(['Activerecord Default is not connected'])
       end
     end
   end
